db5d080077a4c4484258a4afb47d06b214a0c5fa
vanfigo <rodrigo.loyola@live.com>
2017-11-18 09:59:22 -0600

    adding example for required, defautl and optional parameters

--- videos/typescript/parametros/app.js
+++ videos/typescript/parametros/app.js
@@ -1,1 +1,15 @@
 "use strict";
+function activar(quien, objeto, momento) {
+    if (objeto === void 0) { objeto = "batiseñal"; }
+    var mensaje;
+    if (momento) {
+        mensaje = quien + " activ\u00F3 la " + objeto + " en la " + momento;
+    }
+    else {
+        mensaje = quien + " activ\u00F3 la " + objeto;
+    }
+    console.log(mensaje);
+}
+;
+activar("Gordon");
+activar("Gordon", "batiseñal", "tarde");
---
+++ videos/typescript/parametros/app.ts
@@ -0,0 +1,11 @@
+function activar( quien:string, objeto:string = "batiseñal", momento?:string) {
+  let mensaje:string
+  if (momento) {
+    mensaje = `${quien} activó la ${objeto} en la ${momento}`;
+  } else {
+    mensaje = `${quien} activó la ${objeto}`;
+  }
+  console.log(mensaje);
+};
+activar("Gordon");
+activar("Gordon", "batiseñal", "tarde");
