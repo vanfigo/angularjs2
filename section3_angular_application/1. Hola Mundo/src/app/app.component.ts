cfe09dac0f2504d30ea6184a6ecbe8a0229ab072
vanfigo <rodrigo.loyola@live.com>
2017-11-19 18:20:56 -0600

    adding first project, a hello world type

---
+++ section3_angular_application/1. Hola Mundo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  nombre:string = "Rodrigo";
+  apellido:string = "Loyola";
+}
