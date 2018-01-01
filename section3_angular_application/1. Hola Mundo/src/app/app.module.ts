2664b91e900c493bc3e1fcd8031a851a0b0b3c53
vanfigo <rodrigo.loyola@live.com>
2018-01-01 14:44:12 -0600

    finalizing structural directives with ngIf and NgFor

--- section3_angular_application/1. Hola Mundo/src/app/app.module.ts
+++ section3_angular_application/1. Hola Mundo/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header.component';
 import { BodyComponent } from './components/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
--- section3_angular_application/1. Hola Mundo/src/app/components/body.component.ts
+++ section3_angular_application/1. Hola Mundo/src/app/components/body.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
 })
 
 export class BodyComponent {
-
+  public show:boolean = false;
+  frase:any = {
+    message: "With a great power comes a great responsability",
+    author: "Ben Parker"
+  }
+  public characters:string[] = ['Spiderman', 'Venom', 'Dr. Octopus']
 }
