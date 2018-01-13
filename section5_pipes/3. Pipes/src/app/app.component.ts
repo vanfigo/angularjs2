464c11d60571e21f39878a648b51390d9431ed55
vanfigo <rodrigo.loyola@live.com>
2018-01-13 14:11:27 -0600

    using pipes

---
+++ section5_pipes/3. Pipes/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  nombre:string = "Rodrigo";
+  arreglo:number[] = [1,2,3,4,5,6,7,8,9];
+  PI:number = Math.PI;
+  a:number = 0.234;
+  salary:number = 1234.5;
+  hero:any = {
+    name: 'Logan',
+    alias: 'Wolverine',
+    age: 500,
+    address: {
+      street: 'first',
+      number: '19'
+    }
+  };
+  promise = new Promise( ( resolve:Function, reject:Function ) => {
+    setTimeout( () => resolve( 'Data is here!' ), 3500 );
+  } );
+  date:Date = new Date();
+}
---
+++ section5_pipes/3. Pipes/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'es'
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
